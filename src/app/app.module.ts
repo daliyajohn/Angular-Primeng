623c464706fafb79df939daba5d6cd590bb99ae1
daliyajohn <daliyajohn75@gmail.com>
2018-11-16 22:19:10 +0530

    Login module generate

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { RouterModule, Routes } from '@angular/router';
+import { AppRoutingModule } from './app.routing';
+import { LoginModule } from 'src/app/login/login.module';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, RouterModule, LoginModule],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
