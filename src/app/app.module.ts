317881adb573769eae325e15da4d1c2527ac7fe7
daliyajohn <daliyajohn75@gmail.com>
2018-11-16 22:30:36 +0530

    Integrate primeng

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,17 @@ import { AppComponent } from './app.component';
 import { RouterModule, Routes } from '@angular/router';
 import { AppRoutingModule } from './app.routing';
 import { LoginModule } from 'src/app/login/login.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, RouterModule, LoginModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    RouterModule,
+    LoginModule,
+    BrowserAnimationsModule
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
